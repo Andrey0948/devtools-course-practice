3d70c0170373c0def72b72d474868d1133497552
Andrey0948 <andrey0948@mail.ru>
2021-03-30 21:57:30 +0300

    fix

--- modules/complex-number/test/test_gapon_andrey_complex_number.cpp
+++ modules/complex-number/test/test_gapon_andrey_complex_number.cpp
@@ -1,28 +1,29 @@
-// Copyright 2021 Gapon Andry
+// Copyright 2021 Gapon Andrey
 
 #include <gtest/gtest.h>
 
 #include <tuple>
-#include <cmath>
 
 #include "include/complex_number.h"
 
-TEST(Gapon_Andrey_ComplexNumberTest, Calculation) {
+TEST(Gapon_Andrey_ComplexNumberTest, Arithmetic_Operations_Corectness) {
     ComplexNumber a(5.5, 2.3);
     ComplexNumber b(6.5, 4.3);
     ComplexNumber c(-4.0, 8.0);
-    ComplexNumber num1 = (a + b) * c;
-    ComplexNumber num2 = (a - b) / c;
     ComplexNumber res(-100.8, 69.6);
     ComplexNumber res2(-0.15, 0.2);
-    EXPECT_EQ(res, num1);
-    EXPECT_EQ(res2, num2);
+
+    ComplexNumber num1 = (a + b) * c;
+    ComplexNumber num2 = (a - b) / c;
+
+    ASSERT_EQ(res, num1);
+    ASSERT_EQ(res2, num2);
 }
 
 typedef testing::TestWithParam<std::tuple<double, double,
     double, double, double, double>>
 Gapon_Andrey_ComplexNumberTest_Param;
-TEST_P(Gapon_Andrey_ComplexNumberTest_Param, associative_Test) {
+TEST_P(Gapon_Andrey_ComplexNumberTest_Param, Associative_Law_Test) {
     ComplexNumber num1(std::get<0>(GetParam()),
         std::get<1>(GetParam()));
     ComplexNumber num2(std::get<2>(GetParam()),
@@ -33,11 +34,11 @@ TEST_P(Gapon_Andrey_ComplexNumberTest_Param, associative_Test) {
     ComplexNumber res1 = (num1 + num2) + num3;
     ComplexNumber res2 = num1 + (num2 + num3);
 
-    EXPECT_EQ(res1, res2);
+    ASSERT_EQ(res1, res2);
 }
 
 TEST_P(Gapon_Andrey_ComplexNumberTest_Param,
-    distributive_Test) {
+    Distributive_Law_Test) {
     ComplexNumber num1(std::get<0>(GetParam()),
         std::get<1>(GetParam()));
     ComplexNumber num2(std::get<2>(GetParam()),
@@ -48,7 +49,7 @@ TEST_P(Gapon_Andrey_ComplexNumberTest_Param,
     ComplexNumber res1 = (num1 + num2) * num3;
     ComplexNumber res2 = num1 * num3 + num2 * num3;
 
-    EXPECT_EQ(res1, res2);
+    ASSERT_EQ(res1, res2);
 }
 
 INSTANTIATE_TEST_CASE_P(, Gapon_Andrey_ComplexNumberTest_Param,
